af41e44b6978e875ff6ce0c5c57396625ea09131
EOguzOzcan <96868012+EOguzOzcan@users.noreply.github.com>
2023-07-31 17:25:58 +0300

    next config fix

--- next.config.js
+++ next.config.js
@@ -1,31 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-
-});
-
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+})
 
 module.exports = withBundleAnalyzer({
   eslint: {
-    dirs: ['.'],
+    dirs: ["."]
   },
-	images: {
-    unoptimized: true,
-  }, 
+  images: {
+    unoptimized: true
+  },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: "",
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-<<<<<<< HEAD
-	output: 'export',
-	assetPrefix: '.',
-=======
-	images: {
-    unoptimized: true,
-  },
-	
->>>>>>> 18024bc95516ccc954f870640f0e421796ee19dc
-});
+  images: {
+    unoptimized: true
+  }
+})
