34a4bc0a05d312dc89280cade763b4e49f749e3d
EOguzOzcan <96868012+EOguzOzcan@users.noreply.github.com>
2023-08-02 14:46:06 +0300

    footer responsive

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
   return (
     <FooterSection>
       <div className='bg-gray-100  p-4'>
-        <div className='grid grid-cols-2 '>
+        <div className='grid sm:grid-cols-2 grid-cols-1 '>
           <div>
             <div className='mb-5'>
               <h4 className='text-4xl font-extrabold text-orange-500'>Get In Touch</h4>
@@ -52,7 +52,7 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className='p-4'>
+          <div className='mt-4'>
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className='mb-4'>
                 <input
