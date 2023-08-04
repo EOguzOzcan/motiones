4f611254cbdc810cbae9db10fd5eb4b29ef9db2b
EOguzOzcan <96868012+EOguzOzcan@users.noreply.github.com>
2023-08-04 13:04:08 +0300

    footer küçültme

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -4,13 +4,13 @@ import Image from "next/image"
 
 const Footer = () => {
   return (
-    <div className='bg-gray-800 p-4 text-white text-center'>
-      <span className='inline-flex items-center text-white'>
+    <div className='bg-gray-800 h-13  text-white p-1 text-center'>
+      <span className='inline-flex items-center text-white mt-2'>
         <Image
           src='/arcglobalchain.png'
           alt='logo'
-          width={44}
-          height={44}
+          width={33}
+          height={33}
           style={{ display: "flex", marginRight: "12px" }}
         />
         {AppConfig.site_name}
