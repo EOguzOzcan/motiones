6facf883c321918dacd5f7e4b524eadde5d8859c
Ömer EREN <erenomer@gmail.com>
2023-08-04 15:57:00 +0300

    about changes

--- src/utils/aboutContent.tsx
+++ src/utils/aboutContent.tsx
@@ -1,7 +1,7 @@
 export const aboutContent = {
-	title: 'About Us At ARC Global Chain,',
+	title: 'About Us At ARC Global Chain',
 	paragraphs: [
-		'we are passionate about providing the finest selection of premium-grade sugar products and top-quality sunflower oil. As a trusted supplier, we offer a range of unique product differentiations and business features to meet your specific needs.',
+		'We are passionate about providing the finest selection of premium-grade sugar products and top-quality sunflower oil. As a trusted supplier, we offer a range of unique product differentiations and business features to meet your specific needs.',
 		"Whether you're a retailer, distributor, or a food manufacturer, Our commitment to excellence sets us apart from the competition, and we are dedicated to providing you with the best products and exceptional service for your business needs.",
 		"Choose us as your partner, and let's sweeten up your business together!"
 	]
