71fb2a8b54f29492de91c1d1f717737a6d83d082
EOguzOzcan <e.oguz.ozcan@gmail.com>
2024-01-02 04:09:09 +0300

    carousel fix

--- src/mantine/components/CardsCarousel.tsx
+++ src/mantine/components/CardsCarousel.tsx
@@ -9,7 +9,6 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules"
 
 import "swiper/css"
-import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
 import "swiper/css/scrollbar"
